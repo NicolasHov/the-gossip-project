99e87c4af6361c8514b4a7e8824401dc5684950e
NicolasHov <kola.hov@gmail.com>
2017-10-25 20:29:22 +0200

    Added index and new + show view

--- app/controllers/gossips_controller.rb
+++ app/controllers/gossips_controller.rb
@@ -1,10 +1,12 @@
 class GossipsController < ApplicationController
 
   def index
+    @gossips = Gossip.all
   end
 
   def show
     @gossip = Gossip.find(params[:id])
+    puts "ceci est mon message #{@gossip}"
   end
 
   def new
@@ -13,14 +15,13 @@ class GossipsController < ApplicationController
   def create
       #render plain: params[:gossip].inspect  #on a un hash avec plain comme key et params[].inspect comme value
       @gossip = Gossip.new(gossip_params)
-
-     @gossip.save
-     redirect_to @gossip
+      @gossip.save
+      redirect_to @gossip
   end
 
   private
     def gossip_params
-      params.require(:gossip).permit(:anonymous_author, :comment)
+      params.permit(:anonymous_author, :comment)
     end
 
 end
