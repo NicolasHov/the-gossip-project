8ebe08b954dc284367efee0d72ab1d79ab44539c
NicolasHov <kola.hov@gmail.com>
2017-10-25 20:47:14 +0200

    Added a welcome view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html: "hello motherfuckers"
+    render html: "hello guys"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 # penser à checker rails routes
 # :format permet d'exporter en json par ex
-  root 'gossips#index'
+  root 'welcome#index'
   resources:gossips #permet d'avoir le CRUD entier dans les routes, l'étape suvante est de faire les views
 end
