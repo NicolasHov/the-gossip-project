5f47c0e72934f33ff8ab04e18320f8c5433c4b49
NicolasHov <kola.hov@gmail.com>
2017-10-26 12:40:22 +0200

    Added methods edit and remove, fixed the navbar

--- app/controllers/gossips_controller.rb
+++ app/controllers/gossips_controller.rb
@@ -6,22 +6,45 @@ class GossipsController < ApplicationController
 
   def show
     @gossip = Gossip.find(params[:id])
-    puts "ceci est mon message #{@gossip}"
   end
 
   def new
+    @gossip = Gossip.new
+  end
+
+  def update
+      @gossip = Gossip.find(params[:id])
+      if @gossip.update(gossip_params)
+        redirect_to @gossip
+      else
+        render "edit"
+      end
+  end
+
+  def edit
+    @gossip = Gossip.find(params[:id])
   end
 
   def create
       #render plain: params[:gossip].inspect  #on a un hash avec plain comme key et params[].inspect comme value
       @gossip = Gossip.new(gossip_params)
-      @gossip.save
-      redirect_to @gossip
+      if @gossip.save
+        redirect_to @gossip
+      else
+        render 'new'
+      end
   end
+
+    def destroy
+      @gossip = Gossip.find(params[:id])
+      @gossip.destroy
+      redirect_to gossips_path
+    end
 
   private
     def gossip_params
       params.permit(:anonymous_author, :comment)
     end
+
 
 end
--- app/models/gossip.rb
+++ app/models/gossip.rb
@@ -1,2 +1,4 @@
 class Gossip < ApplicationRecord
+  validates :anonymous_author, presence: true
+  validates :comment, presence: true, length: { minimum: 5 }
 end
