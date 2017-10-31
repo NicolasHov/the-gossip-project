70fa5e5b9a4ab3ff3acbd9a5cc08b263f29d3626
NicolasHov <kola.hov@gmail.com>
2017-10-31 22:39:49 +0100

    WIP : login not operational yet. Added helper for session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,16 @@
 class SessionsController < ApplicationController
   def new
-    
+  end
+
+  def create
+    moussaillon = Moussaillon.find_by(mail: params[:session][:mail].downcase)
+    if moussaillon && moussaillon.authenticate[params[:session][:password]]
+      log_in moussaillon
+      redirect_to moussaillon 
+    else
+      #flash error
+      flash.now[:danger] = "Invalid credentials"
+      render 'new'
+    end
   end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,6 @@
 module SessionsHelper
+
+  def log_in(moussaillon)
+    session[:moussaillon_id] = moussaillon.id
+  end
 end
